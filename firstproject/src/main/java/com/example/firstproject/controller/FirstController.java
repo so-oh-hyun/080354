c949e99a6a65396d15dd03a8a8713b896039cec3
so-oh-hyun <109723840+so-oh-hyun@users.noreply.github.com>
2024-01-18 12:47:22 +0900

    code update

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
 
-    @GetMapping("/hi")
+    @GetMapping("/bye")
     public String niceToMeetYou(){
         return "greetings";
     }
